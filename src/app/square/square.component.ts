fc845712db006a2b3a1a1fad1c17f0742dbd4dc2
lee-treehouse <leentaylor@gmail.com>
2022-04-10 15:14:04 +1000

    add angular Input (prop type thing) to square component and display on button

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -1,17 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-square',
   template: `
-    <p>
-      {{ rando }}
-    </p>
+    <button>
+      {{ value }}
+    </button>
   `,
   styles: [],
 })
 export class SquareComponent {
-  rando: number = 0;
-  constructor() {
-    setInterval(() => (this.rando = Math.random()), 500);
-  }
+  @Input() value: 'X' | 'O';
 }
